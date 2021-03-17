b282f6d6f53ba0ca4c5672551d42de80d95ced35
P-ADITHYA05 <76908800+P-ADITHYA05@users.noreply.github.com>
2021-03-17 20:53:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var ball;
+var database,position;
+
+function setup(){
+    database = firebase.database();
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+
+    //.ref is used to refer a particular property in database
+    //.onis a listener that helps you to listen the changed values
+    var ballPosition = database.ref('soccer/position')
+    ballPosition.on("value",readPosition,showError)
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+    database.ref('soccer/position').set({
+        'x':position.x+x,
+        'y':position.y+y
+    })
+}
+function readPosition(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+}
+function showError(){
+    console.log("error in writing to the database")
+}
